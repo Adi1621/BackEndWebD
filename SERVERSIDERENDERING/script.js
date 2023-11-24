62ef4f44ec8598d71e64f43a19e3ed4129717e47
unknown <aditya1621.be21@chitkara.edu.in>
2023-11-24 22:23:41 +0530

    Server side redering

---
+++ SERVERSIDERENDERING/script.js
@@ -0,0 +1,30 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+// app.use(express.static(path.join(__dirname,"static")))
+// app.use(express.urlencoded({extended:true})); 
+// app.use(express.json());
+app.set('view engine', 'hbs');
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        name: "Aditya Kanswal",
+        age: 21
+    });
+});
+
+app.listen(4444, () => {
+    console.log("server started at port 4444");
+});
+
+
+
+/*
+Step 1 Require express
+Step 2 Calling express 
+Step 3 Starting Server
+*/
