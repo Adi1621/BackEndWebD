0ec106dbba26e3efb34e58db7ab94890d14f5c93
unknown <aditya1621.be21@chitkara.edu.in>
2023-11-26 19:18:10 +0530

    GET opoeration done

--- BEE_PROJECT/index.js
+++ BEE_PROJECT/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const tasks=require("./MOCK_DATA.json");
 
 const app = express();
 
-app.listen(7000, () => { console.log("server started") });
+app.get("/", (req, res) => {
+    return res.send("Hi, This is Home Page of Task Manager");
+});
 
-app.get("/", (req,res) => {
-    return res.send("Hi form server");
+app.get("/tasks", (req, res)=>{
+    const html = `
+    <ul>
+       ${tasks.map((task) => `<li>${task.task}</li>`).join(`<br>`)}
+    </ul>
+    `
+    return res.send(html);
 })
+
+app.listen(7000, () => { console.log("server started") });
