f56dac31a6166ee5be7b6c4ca35acd782ce18a0e
unknown <aditya1621.be21@chitkara.edu.in>
2023-11-26 17:54:33 +0530

    MOCK file created

--- BEE_PROJECT/index.js
+++ BEE_PROJECT/index.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 
 app.listen(7000, () => { console.log("server started") });
+
+app.get("/", (req,res) => {
+    return res.send("Hi form server");
+})
