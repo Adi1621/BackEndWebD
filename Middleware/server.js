2493e7e695922e7a20ab6970905b865afd81e360
unknown <aditya1621.be21@chitkara.edu.in>
2023-12-04 20:13:02 +0530

    Creating More Middlewares

--- Middleware/server.js
+++ Middleware/server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 app.use(middleware1);
-function middleware1(req,res,next){
+app.use(middleware2);
+app.use(middleware3);
+
+function middleware1(req, res, next) {
+    console.log("running middleware1 ")
+    next();
+}
+function middleware2(req, res, next) {
+    console.log("running middleware1 ")
+    next();
+}
+function middleware3(req, res, next) {
     console.log("running middleware1 ")
     next();
 }
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     console.log("running /");
     res.send("home");
 })
 
 
-app.listen(4444,()=>{
+app.listen(4444, () => {
     console.log("Server Started");
 })
